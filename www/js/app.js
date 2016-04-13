4ab4157c9a0be12861728948a05090c8eaa9c2b7
smashingpat <patt.gerr@gmail.com>
2016-04-13 16:02:23 +0200

    add css styling and seperated API calls

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,6 @@
 angular
     .module('starter', ['ionic'])
-    .config(function($httpProvider) {
-        // delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    })
+    .factory('dataservice', dataServiceFactory)
     .run(function($ionicPlatform, $http) {
         $ionicPlatform.ready(function() {
             if(window.cordova && window.cordova.plugins.Keyboard) {
@@ -16,10 +14,21 @@ angular
     })
     .controller('mainController', mainController);
 
-function mainController($scope, $http) {
-    // import API
-    $http.get('/api/people').success(function(results) {
-        $scope.platforms = results
-    });
+function dataServiceFactory($http) {
+    function retrieveData() {
+        return $http.get('/api/people').then(function(response) {
+            return response.data;
+        });
+    }
 
+    return {
+        retrieveTeam: retrieveData
+    }
+}
+
+function mainController($scope, $http, dataservice) {
+    $scope.platforms = [];
+    dataservice.retrieveTeam().then(function(data) {
+        $scope.platforms = data;
+    })
 }
