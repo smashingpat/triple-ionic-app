07d4976c8798d4a7a8881601ba199bc476e412d5
smashingpat <patt.gerr@gmail.com>
2016-04-15 12:40:36 +0200

    basic structure for views set, transforming data and caching

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,5 @@
 angular
-    .module('starter', ['ionic'])
-    .factory('dataservice', dataServiceFactory)
+    .module('starter', ['ionic', 'ngAnimate'])
     .run(function($ionicPlatform, $http) {
         $ionicPlatform.ready(function() {
             if(window.cordova && window.cordova.plugins.Keyboard) {
@@ -12,23 +11,93 @@ angular
             }
         });
     })
-    .controller('mainController', mainController);
+    .config(angularConfig)
+    .factory('dataservice', dataServiceFactory)
+    .controller('teamController', teamController)
+    .controller('memberController', memberController);
+
+function angularConfig($stateProvider, $urlRouterProvider) {
+    // if 404 then go to '/page'
+    $urlRouterProvider.when('/team/member', '/team');
+
+    // Stateprovider
+    // ------------------------------
+    $stateProvider
+        .state('app', {
+            abstract: true,
+            templateUrl: 'templates/main.tpl.html'
+        })
+        .state('app.team', {
+            url: '/team',
+            views: {
+                'team@app': {
+                    controller: 'teamController as team',
+                    templateUrl: 'templates/teamView.tpl.html'
+                }
+            }
+        })
+        .state('app.team.member', {
+            url: '/member/{id}',
+            views: {
+                'team@app': {
+                    controller: 'memberController as member',
+                    templateUrl: 'templates/teamMember.tpl.html'
+                }
+            }
+        })
+
+}
 
 function dataServiceFactory($http) {
-    function retrieveData() {
-        return $http.get('/api/people').then(function(response) {
-            return response.data;
-        });
+    // store data here for when controller refreshes
+    var data;
+    // uses cached data instead of attempting to retrieving data from server every time
+    function getData(callback) {
+        if(data) {
+            callback(data);
+        } else {
+            $http.get('/api/people').success(function(response) {
+                var idNum = 1000;
+                var transformedData = [];
+                // transform data
+                angular.forEach(response, function(object, platform) {
+                    angular.forEach(object, function(member) {
+                        member.id = idNum;
+                        member.platform = platform;
+                        member.photoUrl = member.photoUrl && 'http://assessments.wearetriple.com/photos/' + member.photoUrl || 'img/profile-icon.png';
+                        transformedData.push(member)
+                        idNum++;
+                    })
+                })
+                callback(data = transformedData);
+            });
+        }
     }
 
     return {
-        retrieveTeam: retrieveData
-    }
+        getData: getData
+    };
 }
 
-function mainController($scope, $http, dataservice) {
-    $scope.platforms = [];
-    dataservice.retrieveTeam().then(function(data) {
-        $scope.platforms = data;
+function teamController($http, dataservice) {
+    var vm = this;
+    vm.members;
+    dataservice.getData(function(data) {
+        vm.members = data;
+    })
+}
+
+function memberController($stateParams, dataservice) {
+    var vm = this;
+    var id = $stateParams.id;
+
+    vm.member;
+
+    dataservice.getData(function(data) {
+        var filteredData = data.filter(function(value) {
+            return value.id == id
+        })[0];
+        console.log(filteredData);
+        vm.data = filteredData;
     })
 }
